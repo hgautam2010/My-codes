46171bb4ab01ebfe5ebb20d882a5240df1f24211
hgautam2010 <hgautam2010@gmail.com>
2019-01-15 17:58:43 +0530

    Create TADELIVE.cpp

---
+++ CodeChef/practice/TADELIVE.cpp
@@ -0,0 +1,57 @@
+//Hader files
+#include <bits/stdc++.h>
+
+//Macros
+#define REP(i, n) for (i = 0; i < n; i++)
+#define REPo(i, n) for (i = n; i >= 0; i--)
+#define REP2(i, m, n) for (i = m; i < n; i++)
+#define mp(a, b) make_pair(a, b)
+#define pb(a) push_back(a)
+#define mod 1000000007
+#define pp pair<int, pair<ll, int> >
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vp vector<pair<int, int> >
+#define vpp vector<pp>
+#define sc second
+#define fs first
+#define Himanshu ios_base::sync_with_stdio(false);
+#define Gautam cin.tie(NULL);
+
+using namespace std;
+
+// directions
+const int fx[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+const int fxx[8][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+//typedefs
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> ii;
+typedef pair<ll, ll> iill;
+
+int main()
+{
+    Himanshu Gautam
+    
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+    #endif
+    
+	ll i, j, k, a, b, n, m, t, ans;
+	string s;
+	
+	cin >> n >> a >> b;
+	
+	vi A(n),B(n);
+	
+	REP(i,n) cin >> A[i];
+	REP(i,n) cin >> B[i];
+	
+	
+	
+	return 0;
+}
